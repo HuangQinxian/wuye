abd36c19344835073e9507c3589e62d7b2207b7e
SUPERHQX <2474658048@qq.com>
2017-08-21 15:15:08 +0800

    。

--- wuye_git/src/com/neusoft/wuye/baseinfo/controller/BuildingTypeController.java
+++ wuye_git/src/com/neusoft/wuye/baseinfo/controller/BuildingTypeController.java
@@ -1,10 +1,14 @@
 package com.neusoft.wuye.baseinfo.controller;
 
+import java.util.List;
+
 import javax.annotation.Resource;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.neusoft.wuye.baseinfo.model.BuildingTypeModel;
 import com.neusoft.wuye.baseinfo.service.IBuildingTypeService;
@@ -25,6 +29,18 @@ public class BuildingTypeController {
 		System.out.println("进来了");
 		buildingTypeService.add(btm);
 		return "/buildingtype/main.html";
+	}
+	
+	@RequestMapping(value="/get")
+	@ResponseBody
+	public BuildingTypeModel get(@RequestParam(required=true) int typeNo) throws Exception {
+		return buildingTypeService.get(typeNo);
+	}
+	
+	@RequestMapping("/getListByAllWithPage")
+	@ResponseBody
+	public List<BuildingTypeModel> getListByAllWithPage(@RequestParam(defaultValue="5")int rows,@RequestParam(defaultValue="1") int page) throws Exception{
+		return buildingTypeService.getListByAllWithPage(rows, page);
 	}
 	
 }
